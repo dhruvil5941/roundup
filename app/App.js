2a5470e1ba0be00ff137c3386c5cc0db89befbad
dhruvil5941 <dhruvil5941@gmail.com>
2021-04-17 19:21:54 +0530

    Remove Warnings

--- app/App.js
+++ app/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
-import NavigatorStack from './navigation/StackNavigation';
+import NavigatorStack from './Navigation/StackNavigation';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreLogs(['Warning: ...']);
+LogBox.ignoreAllLogs();
+
 class App extends Component {
   render() {
-    return (
-        <NavigatorStack />
-    );
+    return <NavigatorStack />;
   }
 }
 
